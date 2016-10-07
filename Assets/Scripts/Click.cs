752a8845bea67cb2435dd226e3e2096b490be858
Adam Morris <amorris2003@gmail.com>
2016-10-07 19:08:37 +1000

    Clicker Game

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+	public UnityEngine.UI.Text gpc;
+	public UnityEngine.UI.Text goldDisplay;
+	public float gold = 0;
+	public int goldperclick = 1;
+
+	void Update() {
+		goldDisplay.text = "Gold: " + gold;
+		gpc.text = "GPC: " + goldperclick;
+	}
+
+	public void Clicked() {
+		gold += goldperclick;
+	}
+}
---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeManager : MonoBehaviour {
+
+	public Click click;
+	public UnityEngine.UI.Text itemInfo;
+	public float cost;
+	public int count = 0;
+	public int clickPower;
+	public string itemName;
+	private float _newCost;
+
+	void Update(){
+		itemInfo.text = itemName + "\nCost: " + cost + "\nPower: +" + clickPower;
+	}
+
+	public void PurchasedUpgrade(){
+		if(click.gold >= cost) {
+			click.gold -= cost;
+			count += 1;
+			click.goldperclick += clickPower;
+			cost = Mathf.Round (cost * 1.15f);
+			_newCost = Mathf.Pow (cost, _newCost = cost);
+		}
+	}
+}
