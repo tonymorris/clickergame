8b56b50cfc62f65399efa7a9bb8a1bd3a1577e57
Adam Morris <amorris2003@gmail.com>
2016-10-08 12:04:49 +1000

    Update

---
+++ Assets/Scripts/CurrencyConverter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrencyConverter : MonoBehaviour {
+
+	private static CurrencyConverter instance;
+	public static CurrencyConverter Instance{
+		get{
+			return instance;
+		}
+	}
+
+	void Awake(){
+		CreateInstance ();
+	}
+
+	void CreateInstance(){
+		if(instance == null) {
+			instance = this;
+		}
+	}
+
+	public string GetCurrencyIntoString(float valueToConvert, bool currencyPerSec, bool currencyPerClick){
+		string converted;
+		if(valueToConvert >= 1000000) {
+			converted = (valueToConvert / 1000000f).ToString("f3") + " Mil";
+	}else if (valueToConvert >= 1000){
+		converted = (valueToConvert / 1000f).ToString("f3") + " K";
+	}else {
+		converted = "" + valueToConvert;
+	}
+
+	if(currencyPerSec == true) {
+			converted = converted + " gps";
+	}
+
+	if(currencyPerClick == true) {
+			converted = converted + " gpc";
+	}
+	return converted;
+}
+}
