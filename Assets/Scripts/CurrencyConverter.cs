73f9a4d6a038c7fd9c83aa4199cba6f17e744617
Joshua Morris <joshuajohnmorris@gmail.com>
2016-10-08 14:25:36 +1000

    Currency converter improvement

--- Assets/Scripts/CurrencyConverter.cs
+++ Assets/Scripts/CurrencyConverter.cs
@@ -20,12 +20,17 @@ public class CurrencyConverter : MonoBehaviour {
 		}
 	}
 
+/*
+  Abbreviate money values with thousands, millions and billions
+*/
 	public string GetCurrencyIntoString(float valueToConvert, bool currencyPerSec, bool currencyPerClick){
 		string converted;
 		if(valueToConvert >= 1000000) {
-			converted = (valueToConvert / 1000000f).ToString("f2") + " Mil";
+			converted = (valueToConvert / 1000000f).ToString("f2") + " M";
 		}else if (valueToConvert >= 1000){
 			converted = (valueToConvert / 1000f).ToString("f2") + " K";
+		}else if (valueToConvert >= 1000000000){
+			converted = (valueToConvert / 1000000000f).ToString("f2") + " B"
 		}else {
 			converted = "" + Mathf.Round(valueToConvert);
 		}
